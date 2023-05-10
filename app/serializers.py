b96fc950ba3dda57ba0eb7a3167bb188c6e0b023
Razi Ahmad <mail2razi4u@gmail.com>
2023-05-10 12:39:25 +0530

    date validation applied in hackathon creattion

--- app/serializers.py
+++ app/serializers.py
@@ -3,6 +3,10 @@ from .models import *
 from django.contrib.auth.models import User
 
 class HackathonSerializer(serializers.ModelSerializer):
+    def validate(self,data):
+        if(data['start_datetime']>=data['end_datetime']):
+            raise serializers.ValidationError("End Date Time Must Be After Start Date Time")
+        return data
     class Meta():
         model = Hackathon
         fields = '__all__'
@@ -11,8 +15,7 @@ class ParticipantSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(default =  serializers.CurrentUserDefault(),queryset=User.objects.all())
     class Meta():
         model= Participant
-        fields = '__all__'
-        
+        fields = '__all__'    
         
 class SubmissionSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(default=serializers.CurrentUserDefault(),queryset=User.objects.all())
